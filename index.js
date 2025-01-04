c01fe4fa1df3210d8de4e55d971dbc74f4db5249
abrar0349 <abrar0349khan@gmail.com>
2025-01-04 12:19:40 +0500

    every route is commit

--- index.js
+++ index.js
@@ -1,35 +1,31 @@
 const express = require('express');
-// const cors = require('cors');
-// const cookieParser = require('cookie-parser')
-// require('dotenv').config()
-// const connectDb = require('./config/db')
+const cors = require('cors');
+const cookieParser = require('cookie-parser')
+require('dotenv').config()
+const connectDb = require('./config/db')
 const router = require('./routers/index')
 
 const app = express()
 
-// app.use(express.json())
+app.use(express.json())
 
-// app.use(cookieParser())
+app.use(cookieParser())
 
 
-// app.use(cors({
-//     origin : process.env.FRONTEND_URL,
-//     credentials : true
-// }))
-
+app.use(cors({
+    origin : process.env.FRONTEND_URL,
+    credentials : true
+}))
 // console.log(router)
 app.use("/api" , router)
 
 
 const PORT = process.env.PORT 
-
 // console.log(PORT)
+connectDb().then(
+    app.listen(PORT , () => {
+        console.log('server is running')
+        console.log('DataBase is Connected')
+    })
+)
 
-// connectDb().then(
-
-// )
-
-app.listen(PORT , () => {
-    console.log('server is running')
-    // console.log('DataBase is Connected')
-})

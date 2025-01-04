04b2a0ff67963f27313f04a12992e153214d0145
abrar0349 <abrar0349khan@gmail.com>
2025-01-04 11:54:27 +0500

    remove database

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const cors = require('cors');
 const cookieParser = require('cookie-parser')
 require('dotenv').config()
-const connectDb = require('./config/db')
+// const connectDb = require('./config/db')
 const router = require('./routers/index')
 
 const app = express()
@@ -10,6 +10,8 @@ const app = express()
 app.use(express.json())
 
 app.use(cookieParser())
+
+
 app.use(cors({
     origin : process.env.FRONTEND_URL,
     credentials : true
@@ -20,10 +22,11 @@ app.use("/api" , router)
 
 const PORT = process.env.PORT 
 // console.log(PORT)
-connectDb().then(
-    app.listen(PORT , () => {
-        console.log('server is running')
-        console.log('DataBase is Connected')
-    })
-)
+// connectDb().then(
 
+// )
+
+app.listen(PORT , () => {
+    console.log('server is running')
+    // console.log('DataBase is Connected')
+})
